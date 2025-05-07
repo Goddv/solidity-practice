2c47834439da99d25323c59e351643b85aa2eb32
Goddv <troyan.roma1998@gmail.com>
2025-05-07 23:02:20 +0200

    Update SimpleBank.sol
    Fixed version

--- Learning/SimpleBank.sol
+++ Learning/SimpleBank.sol
@@ -2,15 +2,21 @@
 pragma solidity >=0.4.23 <0.9.0;
 
 contract getBank {
+    mapping (address => uint) public balanceOf;
+
     function getBalance(address _addr) public view returns (uint balance){
-        balance = address(_addr).balance;
-    }
-    function depositFunds(address payable _addr, uint amountToDeposit) public {
-        _addr.transfer(amountToDeposit);
+        balance = balanceOf[_addr];
     }
 
-    function withdrawFund(address payable _withdrawAddr, uint amountToWithdraw) private {
-        _withdrawAddr.transfer(amountToWithdraw);
+    function depositFunds() public payable {
+        require (msg.value > 0, "Amount shouldn't be zero");
+        balanceOf[msg.sender] += msg.value;
     }
-        
+
+    function withdrawFunds(uint amount) public payable {
+        require (balanceOf[msg.sender] >= amount, "Can't withdraw more than you own");
+        balanceOf[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    
+    }
 }
