f4bc407b322cf0a606abe0b37fe1f57e4b802603
Goddv <troyan.roma1998@gmail.com>
2025-05-12 01:14:55 +0200

    Initial SimpleWallet implementation

---
+++ Learning/Contracts/SimpleWallet.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.23 <0.9.0;
+
+contract SimpleWallet {
+    //Initialize owner's public adress, map adresses and allowance;
+    address public owner;
+    mapping (address => uint) private balances;
+    mapping (address => bool) public hasAccess;
+
+    //Make deployer an owner of contract
+    constructor() {
+        owner = msg.sender;
+        hasAccess[owner] = true;
+    }
+
+    //Only owner modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You must be the owner to do that");
+        _;
+    }
+
+    //Allowance modifier
+    modifier onlyAllowed() {
+        require(hasAccess[msg.sender], "This account doesn't have access.");
+        _;
+    }
+
+    //Give access to wallet
+    function giveAccess (address _to) public onlyOwner (){
+        hasAccess[_to] = true;
+    }
+
+    //Remove acces from wallet
+    function removeAccess (address _to) public onlyOwner () {
+        hasAccess[_to] = false;
+    }
+
+    //Get wallet's status
+    function getStatus (address _who) public view returns (string memory) {
+        if (hasAccess[_who] == true && _who == owner) {
+            return "Owner";
+        }
+        else if (hasAccess[_who] == true && _who != owner) {
+            return "User with access";
+        }
+        else if (!hasAccess[_who]){
+            return "No access";
+        }
+    }
+
+    //Change owner
+    function changeOwner (address _newOwner) private onlyOwner () {
+        owner = _newOwner;
+    }
+
+    //Deposit
+    function depositFunds () public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    //Withdraw
+    function withdrawFunds (uint256 _amount) onlyOwner() public {
+        require(hasAccess[msg.sender], "This account doesn't have access.");
+        balances[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+
+    //Get wallet balance
+    function getBalance (address _who) public view returns (uint256) {
+        return balances[_who];
+    }
+}
